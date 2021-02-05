6978a489dbf8c3d6bbe6f30061c0d7112b18216d
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-02-05 12:25:41 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Ashkan Amiri
+ * Date:  2021-02-05
+ * Time:  12:15
+ * Project: shoesWebbShop
+ * Copyright: MIT
+ */module shoesWebbShop {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+
+    opens sample;
+}
