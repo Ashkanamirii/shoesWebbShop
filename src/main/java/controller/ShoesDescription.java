91135206cbd2891d5bf93f63aa80b7c7e0508c01
hodeiez <hodeinaiz@hotmail.com>
2021-02-14 23:35:56 +0100

    linked shoesDescription with webbShoPage

--- src/main/java/controller/ShoesDescription.java
+++ src/main/java/controller/ShoesDescription.java
@@ -1,5 +1,11 @@
 package controller;
 
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import modell.Shoes;
+import utils.UserLogin;
+
 /**
  * Created by Hodei Eceiza
  * Date: 2/14/2021
@@ -8,4 +14,40 @@ package controller;
  * Copyright: MIT
  */
 public class ShoesDescription {
+    public ImageView shoesImage;
+    public Label brandL;
+    public Label sizeL;
+    public Label colorL;
+    public Label categoryL;
+    public Label onStockL;
+    public Label priceL;
+    public Button addToCartB;
+    public Spinner<Integer> quantityS;
+
+    public void initialize(){
+    }
+    public void setData(Shoes shoesData, ObservableList<Shoes> shoppingCart,Label totalPriceL){
+
+        if(!UserLogin.getIsLogged())
+            addToCartB.setVisible(false);
+
+
+        brandL.setText(shoesData.getBrand());
+        colorL.setText(shoesData.getColor());
+        priceL.setText(shoesData.getPrice()+"");
+        sizeL.setText(shoesData.getSize()+"");
+        onStockL.setText((shoesData.getQuantity()>0)?"ON STOCK":"NO AVAILABLE");
+
+        addToCartB.setOnMouseClicked(e-> {
+
+            shoesData.setQuantity(quantityS.getValue());
+            shoppingCart.add(shoesData);
+            //update Label
+            totalPriceL.setText(shoppingCart.stream().map(Shoes::getPrice).reduce(0,(a,b)->a+b).toString());
+        });
+
+        quantityS.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,shoesData.getQuantity()));
+
+
+    }
 }
--- src/main/java/controller/WebbshopPage.java
+++ src/main/java/controller/WebbshopPage.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -13,6 +14,7 @@ import modell.Shoes;
 import org.controlsfx.control.Rating;
 import utils.UserLogin;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +28,6 @@ public class WebbshopPage {
     public TableColumn c5;
     public TableColumn c6;
     public TableColumn c7;
-    public TableView tableTest;
     public ComboBox showColors;
     public Button showTable;
     public TextField searchField;
@@ -35,81 +36,85 @@ public class WebbshopPage {
 
     public PasswordField passF;
     public TextField email;
-    public Rating ratingTest;
+
     public TableView shoppingCartView;
-    public TableColumn cartId;
     public TableColumn cartPrice;
     public TableColumn cartBrand;
     public TableColumn cartQuantity;
     public Label totalPriceL;
+    public TableView shoesTable;
+    public ComboBox showBrands;
+    public ComboBox showCategories;
+    public Pane loginPane;
+    public Button toRegister;
+    public Pane shoppinCartP;
+    public Button confirmOrder;
+    public Pane shoesDescriptionP;
 
 
     private ObservableList<Shoes> shoesList;
-    private FilteredList<Shoes> shoesFiltered;
     private ObservableList<Shoes> shoppingCart;
 
     public void initialize() {
 
-        //testing shopping cart
+        if(!UserLogin.getIsLogged())
+            shoppinCartP.setVisible(false);
+        else{
+            loginPane.setVisible(false);
+            loginL.setText("you are logged as " ); //we get here the customer name
+        }
+
+
+
+
+        //shoping cart
         shoppingCart = FXCollections.observableArrayList();
-        cartId.setCellValueFactory(new PropertyValueFactory("id"));
         cartPrice.setCellValueFactory(new PropertyValueFactory("price"));
         cartBrand.setCellValueFactory(new PropertyValueFactory("brand"));
         cartQuantity.setCellValueFactory(new PropertyValueFactory("quantity"));
 
         shoppingCartView.setItems(shoppingCart);
-        cartQuantity.setEditable(true);
 
 
-        //testing rating
-        ratingTest.setOrientation(Orientation.VERTICAL);
-
-
-        //testing the log in with the singleton
+        // log in with the singleton
         startLogInB.setOnAction(e -> {
-
             UserLogin.getInstance(email.getText(), passF.getText());
             if (UserLogin.getIsLogged()) {
-                loginL.setText("YOU are Logged IN!!!!");
-                ratingTest.setVisible(true);
+                loginL.setText("You are logged in!!!!");
+                shoppinCartP.setVisible(true);
             }
-            ;
+
         });
 
 
-        System.out.println(QueryExec.returnList("SELECT * FROM shoes;").toString());
+     //Show in table-> brand, color, size,price,rating,category,quantity
+        //TODO: implement rating and category to shoes.
 
-        tableTest.setVisible(false);
-
-
-        //QueryExec q = new QueryExec();
-//c1.setCellValueFactory(new PropertyValueFactory("id"));
-        c2.setCellValueFactory(new PropertyValueFactory("size"));
-        c3.setCellValueFactory(new PropertyValueFactory("shoes_number"));
-        c4.setCellValueFactory(new PropertyValueFactory("brand"));
-        c5.setCellValueFactory(new PropertyValueFactory("color"));
-        c6.setCellValueFactory(new PropertyValueFactory("price"));
+        c1.setCellValueFactory(new PropertyValueFactory("brand"));
+        c2.setCellValueFactory(new PropertyValueFactory("color"));
+        c3.setCellValueFactory(new PropertyValueFactory("size"));
+        c4.setCellValueFactory(new PropertyValueFactory("price"));
+        //c5.setCellValueFactory(new PropertyValueFactory("rating"));
+        //c6.setCellValueFactory(new PropertyValueFactory("category"));
         c7.setCellValueFactory(new PropertyValueFactory("quantity"));
 
-        c1.setText("id");
-        c2.setText("Size");
-        c3.setText("Shoes_number");
-        c4.setText("Brand");
-        c5.setText("Color");
-        c6.setText("Price");
-        c7.setText("Quantity");
+        c1.setText("Brand");
+        c2.setText("Color");
+        c3.setText("Size");
+        c4.setText("Price");
+        c5.setText("Rating");
+        c6.setText("Category");
+        c7.setText("On stock");
 
 
         try {
-            showColors.itemsProperty().setValue(QueryExec.returnQueryToList("select distinct color from shoes;"));
-
+            showColors.itemsProperty().setValue(QueryExec.getColorsList());
+            showCategories.itemsProperty().setValue(QueryExec.getCategoriesList());
+           // showBrands.itemsProperty().setValue(QueryExec.returnQueryToList("select distinct from category"));
             //using obs list
 
-            shoesList = QueryExec.returnList("SELECT shoes.id as id,size,shoes_number," +
-                    "br.name as FK_brand_id,color,price,quantity\n" +
-                    " FROM shoes\n" +
-                    " join brand br on br.id =FK_brand_id;");
-            tableTest.setItems(shoesList);
+            shoesList = QueryExec.getShoesList();
+            shoesTable.setItems(shoesList);
 
 
         } catch (Exception e) {
@@ -118,58 +123,45 @@ public class WebbshopPage {
         }
 
 
-//using observable list
+
 
         showColors.valueProperty().addListener(((observableValue, o, t1) ->
-                tableTest.setItems(filteredList(shoesList, t1.toString()))));
+                shoesTable.setItems(filteredList(shoesList, t1.toString()))));
         searchField.textProperty().addListener(((observableValue, s, t1) ->
-                tableTest.setItems(filteredList(shoesList, t1))));
+                shoesTable.setItems(filteredList(shoesList, t1))));
 
         //select by click this can call to addToCart (or display a new pane asking for confirm to add to cart)
-        tableTest.setOnMouseClicked(e -> {
-            if (e.getClickCount() == 2) {
-                System.out.println(((Shoes) tableTest.getSelectionModel().getSelectedItem()).getId());
-                Alert a = new Alert(Alert.AlertType.CONFIRMATION);
-                a.setHeaderText("Do you want to add this item to the cart?");
+        shoesTable.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2)
+                loadShoesDesc(((Shoes) shoesTable.getSelectionModel().getSelectedItem()),shoppingCart,totalPriceL);
 
-
-                //TODO: fix userPrivileges mess
-                if (UserLogin.getIsLogged() == true) {
-                    a.setContentText("you choose this id ->" + ((Shoes) tableTest.getSelectionModel().
-                            getSelectedItem()).getId());
-
-                    //add to cart when click OK
-                    a.showAndWait().ifPresent(response -> {
-                        if (response == ButtonType.OK) {
-                            Shoes newShoes = shoesList.get(((Shoes) tableTest.getSelectionModel().
-                                    getSelectedItem()).getId() - 1);
-                            newShoes.setQuantity(1);
-                            shoppingCart.add(newShoes);
-                            //sum price
-                            totalPriceL.setText(shoppingCart.stream().map(Shoes::getPrice).
-                                    reduce(0, Integer::sum).toString());
-
-                        }
-                    });
-                } else {
-                    a.setContentText("you choose this id ->" +
-                            ((Shoes) tableTest.getSelectionModel().getSelectedItem()).getId() + "\n" +
-                            "YOU ARE NOT LOGGED IN, go and logg in to enjoy your shopping");
-                    a.show();
-                }
-
-            }
         });
 
         showTable.setOnAction(e -> {
-            tableTest.setVisible(true);
-            // tableTest.setItems(shoesList);
-            // shoesFiltered.setPredicate(shoesPredicate(""));
+            shoesTable.setItems(shoesList);
         });
 
     }
 
 
+    //adds the shoes description panel
+    public void loadShoesDesc (Shoes shoesData, ObservableList<Shoes> shoppingCart, Label totalPrice)  {
+        shoesDescriptionP.getChildren().clear();
+        Pane newLoadedPane = null;
+        FXMLLoader loader=new FXMLLoader();
+        try {
+            loader.setLocation(getClass().getResource("/shoesDescription.fxml"));
+            newLoadedPane=loader.load();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ShoesDescription controller=loader.getController();
+        controller.setData(shoesData,shoppingCart,totalPriceL);
+        shoesDescriptionP.getChildren().add(newLoadedPane);
+    }
+
+//Now searches only color and brand
     private boolean isFound(Shoes shoesData, String searchText) {
         return (shoesData.getColor().toLowerCase().contains(searchText)
                 || shoesData.getBrand().toLowerCase().contains(searchText));
